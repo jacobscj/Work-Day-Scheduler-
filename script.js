943e8dbac9cecfbe3c006d7f61e891a8c31eff99
Connor Jacobs <connorjacobs222@gmail.com>
2021-03-08 17:07:10 -0500

    switching times to military for better use of function

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ $(document).ready(function () {
     $("#10am .description").val(localStorage.getItem("10am"));
     $("#11am .description").val(localStorage.getItem("11am"));
     $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#1pm .description").val(localStorage.getItem("13pm"));
+    $("#2pm .description").val(localStorage.getItem("14pm"));
+    $("#3pm .description").val(localStorage.getItem("15pm"));
+    $("#4pm .description").val(localStorage.getItem("16pm"));
+    $("#5pm .description").val(localStorage.getItem("17pm"));
 
 
     // adding function to color code each time block depending on current time of day
