236a911e5c1b87a466b5aa05209527188f3ff690
Connor Jacobs <connorjacobs222@gmail.com>
2021-03-07 21:46:20 -0500

    adding Jquery to get items from local storage

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 $(document).ready(function () {
-    
-    $(".saveBtn").on("click", function() {
-        // variable to differentiate between time blocks 
+    // using momentjs to display current date & time in header
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+    // this function runs when the save button is clicked
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        // variable to differentiate between time blocks
+        // "this" refers to the save button being clicked
+        // parent is the row with the time id
         var timeRow = $(this).parent().attr("id");
         // variable to store user input in text area as value
+        // "this" refers to the save button being clicked
+        // sibling is the text area 
         var textInput = $(this).siblings(".description").val();
 
-        window.localStorage.setItem(timeRow, textInput);
-
+        // saving time of day as key, and user input as value to the local storage
+        localStorage.setItem(timeRow, textInput);
     })
 
+    //displaying the value saved in the text area by getting it from where it was set in local storage
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+
+})
 
 
 
@@ -29,4 +48,4 @@ $(document).ready(function () {
 
 
 
-});
+
